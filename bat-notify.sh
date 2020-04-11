630bdd7ddb79cd8b9d35a673a31727f953d70dda
enigmatic-system <admin@enigmaticsystem.xyz>
2020-04-11 06:57:04 -0700

    Create bat-notify.sh

---
+++ bat-notify.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Battery Notification Script
+# Version 2.0
+
+while true  
+do
+
+BatteryCapacity=`cat /sys/class/power_supply/BAT0/capacity`
+CriticalAlert=10
+NormalAlert=15
+LowAlert=20
+
+	if [ $BatteryCapacity -le $CriticalAlert ]; then
+		notify-send -u critical "Alert" "Battery Low!!!"
+		sleep 30
+	elif [ $BatteryCapacity -le $NormalAlert ]; then
+		notify-send -u normal "Alert" "Battery is getting low..."
+		sleep 30
+	else
+		if [ $BatteryCapacity -le $LowAlert ]; then
+			notify-send -u low "Alert" "Think about plugging in computer if possible..."
+			sleep 300
+		else
+			sleep 15
+		fi
+	fi
+done 
